87eb6d3d325133788c1f69f93430b1257df72cc2
kiri3914 <t.bektur.2002@gmail.com>
2024-02-01 11:07:31 +0600

    Изменения отображения

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 from accounts.utils import get_data
 
+
+def raspakovka(data):
+    data_list = []
+    for key, value in data['conversion_rates'].items():
+        data_list.append({'currency': key, 'value': 1 / value})
+    return data_list
+
 def index(request):
     data = get_data('KZT')
     context = {
-        'data': data
+        'data': raspakovka(data)
     }
     return render(request, 'mainapp/index.html', context)
 
